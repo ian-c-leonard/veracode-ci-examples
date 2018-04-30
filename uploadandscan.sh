08c97862e639447098fd9adb8a1a857b5fabcc97
Chris Campbell <chris@ctcampbell.com>
2018-04-30 22:01:58 +0100

    Update to include wrapper version variable

--- uploadandscan.sh
+++ uploadandscan.sh
@@ -3,7 +3,8 @@
 DATE=`date +"%Y-%m-%d-%H%M"`
 TEAM_ANALYSISCENTER_ID=$1
 TEAM_ANALYSISCENTER_KEY=$2
-APP_PROFILE_NAME=$3
-FILEPATH=$4
+WRAPPER_LOCATION=$3
+APP_PROFILE_NAME=$4
+FILEPATH=$5
 
-/usr/bin/java -jar --add-modules java.se.ee veracode-wrapper.jar -vid $TEAM_ANALYSISCENTER_ID -vkey $TEAM_ANALYSISCENTER_KEY -action uploadandscan -appname "${APP_PROFILE_NAME}" -createprofile true -filepath $FILEPATH -version $DATE
+/usr/bin/java -jar --add-modules java.se.ee $WRAPPER_LOCATION -vid $TEAM_ANALYSISCENTER_ID -vkey $TEAM_ANALYSISCENTER_KEY -action uploadandscan -appname "${APP_PROFILE_NAME}" -createprofile true -filepath $FILEPATH -version $DATE
